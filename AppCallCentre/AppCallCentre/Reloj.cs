2ac627cb0ae2d77c7262f0f9ba32539a390122cb
MarcosJochatoma <you@example.com>
2017-04-10 00:00:52 -0500

    Clases Reloj y Tiempo

--- AppCallCentre/AppCallCentre/Reloj.cs
+++ AppCallCentre/AppCallCentre/Reloj.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace AppCallCentre
 {
-    class Reloj
+    public class Reloj : Tiempo
     {
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+
+        public Reloj() : base()
+        {
+
+        }
+
+        public int GetTiempo()
+        {
+            base.GetTiempo(out _hours, out _minutes, out _seconds);
+            return 3600 * _hours + 60 * _minutes + _seconds;
+        }
+
+
     }
 }
--- AppCallCentre/AppCallCentre/Tiempo.cs
+++ AppCallCentre/AppCallCentre/Tiempo.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace AppCallCentre
 {
-    class Tiempo
+    public class Tiempo
     {
+        public DateTime _timeNow;
+
+        public Tiempo()
+        {
+            _timeNow = DateTime.Now;
+        }
+         
+        public void GetTiempo(out int hora, out int minutos, out int segundos)
+        {
+            hora = _timeNow.Hour;
+            minutos = _timeNow.Minute;
+            segundos = _timeNow.Second;
+
+        }
+
     }
 }
